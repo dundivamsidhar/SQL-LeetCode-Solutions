a2ae50654e08bffc9aa1215868c22d6cf0848662
Dundi vamsi <dundi.vdr@gmail.com>
2026-02-12 23:24:08 +0530

    Time: 289 ms (40.04%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,11 +1,12 @@
 # Write your MySQL query statement below
 select x,y,z,
-case
+case 
     when x + y > z
     and y + z > x
-    and z + x > y
+    and x + z > y
     then 'Yes'
     else 'No'
-end as triangle 
+end as triangle
 from triangle; 
+
 
